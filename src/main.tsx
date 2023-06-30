77ee77cffbff6142086e5d1d029399070b0a53ff
Gabriel Costa <gabrielcostadev@hotmail.com>
2023-06-30 05:53:15 -0300

    index styled update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
-import './styles/global.scss';
+import './styles/default.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
